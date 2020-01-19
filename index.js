82f28ce6c0b0e642e92703bce69d8512d2fd80e3
Gonk Droid <56324288+LinuxAmGood@users.noreply.github.com>
2020-01-18 22:47:19 -0700

    Update index.js

--- index.js
+++ index.js
@@ -15,11 +15,31 @@ $(function(){
     }
   });
 });
+<script>
+var slideIndex = 1;
+showSlides(slideIndex);
 
-function openNav() {
-  document.getElementById("myNav").style.width = "35%";
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
 
-function closeNav() {
-  document.getElementById("myNav").style.width = "0%";
+function currentSlide(n) {
+  showSlides(slideIndex = n);
 }
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+</script>
