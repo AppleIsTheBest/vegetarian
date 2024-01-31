13a9aecfb4d83b3fd215d95b0289a0460a1ba59a
Todd Smith <45441452+AppleIsTheBest@users.noreply.github.com>
2024-01-31 03:10:26 -0700

    Update index.js

--- Model/index.js
+++ Model/index.js
@@ -22,3 +22,47 @@ $(function(){
     }
   });
 });
+
+function calculateBenefits() {
+    if (programValue == "es-b") {
+      if (stateCode == "CA") {
+        document.getElementById("benefitLabel").innerHTML = "Depending on the type of home, consult the appropriate program: single family home - SFNH California, manufactured home - MH v2, or multi-family home MFNC California.";
+      }
+      else if (stateCode == "FL") {
+        document.getElementById("benefitLabel").innerHTML = "Depending on the type of home, consult the appropriate program: single family home - SFNH Florida or SFNH National, manufactured home - MH v2, or multi-family home MFNC National.";
+      }
+      else if (stateCode == "HI") {
+        document.getElementById("benefitLabel").innerHTML = "Depending on the type of home, consult the appropriate program: single family home - SFNH Pacific, manufactured home - MH v2, or multi-family home MFNC National.";
+      }
+      else if (stateCode == "OR" || stateCode == "WA") {
+        document.getElementById("benefitLabel").innerHTML = "Depending on the type of home, consult the appropriate program: single family home - SFNH Oregon and Washington or SFNH National, manufactured home - MH v2, or multi-family home MFNC Oregon and Washington or MFNC National.";
+      }
+      else if (homeYear < 2023) {
+        document.getElementById("benefitLabel").innerHTML = "Your home is not eligible for a tax rebate under the ENERGYSTAR program.";
+      }
+      else if (homeYear > 2023 && homeYear < 2027) {
+        switch (homeYear) {
+          case 2023:
+            document.getElementById("benefitLabel").innerHTML = "If your home is a: single family home - $2500, manufactured home - $2500, or multi-family home - $500 ($2500 subject to wage requirements).";
+            break;
+          case 2024:
+            document.getElementById("benefitLabel").innerHTML = "Your home is not eligible for a tax rebate under the ENERGYSTAR program.";
+            break;
+          case 2025:
+            document.getElementById("benefitLabel").innerHTML = "Your home is not eligible for a tax rebate under the ENERGYSTAR program.";
+            break;
+          case 2026:
+            document.getElementById("benefitLabel").innerHTML = "Your home is not eligible for a tax rebate under the ENERGYSTAR program.";
+            break;
+          case 2027:
+            document.getElementById("benefitLabel").innerHTML = "Your home is not eligible for a tax rebate under the ENERGYSTAR program.";
+            break;
+          default:
+            break;
+        }
+      }
+      else {
+        document.getElementById("benefitLabel").innerText = "Tax rebate information for your home has not been released by ENERGYSTAR yet.";
+      }
+    }
+}
